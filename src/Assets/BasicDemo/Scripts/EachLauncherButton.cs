777a0fac0dcddf93ea6e6537286a0ed73a6b78f3
aripinky <aripinky@nate.com>
2014-06-11 15:16:44 +0900

    게임 런쳐 완료

---
+++ src/Assets/BasicDemo/Scripts/EachLauncherButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EachLauncherButton : MonoBehaviour 
+{
+	public bool		IsActive = true;
+	public string	GameName;
+
+
+
+	// Start -----------------------------------------
+	void Start () 
+	{
+		dfButton button = GetComponent<dfButton> ();
+		button.Text = GameName;
+
+		button.IsEnabled = IsActive;
+	}
+
+
+
+	// GameSelect ---------------------------
+	public void GameSelect()
+	{
+		Debug.Log ("Go " + GameName);
+	}
+
+}
---
+++ src/Assets/BasicDemo/Scripts/EachResizingUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EachResizingUI : MonoBehaviour 
+{
+
+	// Start ----------------------------------------------
+	void Start () 
+	{
+		transform.localScale = Vector3.one * (Screen.width / 1920f);
+	}
+
+}
